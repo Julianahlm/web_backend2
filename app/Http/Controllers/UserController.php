7eff7e534c558ede3e1a2cda325ddf38b4db9246
Julianahlm <165674015+Julianahlm@users.noreply.github.com>
2024-04-15 18:56:12 +0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,31 +1,24 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Http\Requests\UserRegisterRequest;
 use App\Http\Requests\UserLoginRequest;
-
 use App\Http\Resources\UserResource;
 use App\Models\User;
-
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
-
-use Illuminate\Http\support\Str;
-use Illuminate\Http\support\Facades\Hash;
-
+use Illuminate\support\Str;
+use Illuminate\support\Facades\Hash;
+use Illuminate\support\Facades\Auth;
 use Illuminate\Http\Exceptions\HttpResponseException;
 
 class UserController extends Controller
 {
     public function register(UserRegisterRequest $request): JsonResponse
     {
-        $user = User::create($request->validation());
-
+        $user = User::create($request->validated());
         return (new UserResource($user))->response()->setStatusCode(201);
     }
-
-    public function login(UserLoginRequest $request): userResource
+    public function login(UserLoginRequest $request): UserResource
     {
         $data = $request->validated();
         $user = User::where('email', $data['email'])->first();
@@ -34,10 +27,16 @@ class UserController extends Controller
                 'errors' => [
                     'message' => ['username or password wrong'],
                 ]
-                ],401));
+            ],401));
+        }
+        $user->remember_token = Str::uuid()->toString();
+        $user->save();
+        return new UserResource($user);
     }
-    $user->remember_token = str::uuid()->toString();
-    $user->save();
-    return new UserResources($user);
-  }
+
+    public function get(Request $request): UserResource
+    {
+        $user = Auth::user();
+        return new UserResource($user);
+    }
 }

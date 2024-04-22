086a3f779c385b6b7384ea76b9aa744c93a54620
Julianahlm <165674015+Julianahlm@users.noreply.github.com>
2024-04-22 14:29:18 +0700

    Create ContactsResource.php

---
+++ app/Http/Resources/ContactsResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ContactsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'mobile' => $this->mobile,
+            'street' => $this->street,
+            'city' => $this->city,
+            'state' => $this->state,
+            'zip' => $this->zip,
+            'country' => $this->country,
+            'vat' => $this->vat,
+        ];
+    }
+}

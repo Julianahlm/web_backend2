5c4b3f406b5ada686ed9584a4e4ebc9d7c3ef9de
Julianahlm <165674015+Julianahlm@users.noreply.github.com>
2024-04-22 14:27:27 +0700

    Create ContactsUpdateRequest.php

---
+++ app/Http/Requests/ContactsUpdateRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ContactsUpdateRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return $this->user() != null;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'code' => 'required|string|unique:contacts|max:10',
+            'name' => 'required|string|max:100',
+            'email' => 'required|string|email|unique:contacts|max:100',
+            'phone' => 'nullable|string|max:20',
+            'mobile' => 'nullable|string|max:20',
+            'street' => 'nullable|string',
+            'city' => 'nullable|string',
+            'state' => 'nullable|string',
+            'zip' => 'nullable|string',
+            'country' => 'nullable|string',
+            'vat' => 'nullable|string',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response([
+            'errors' => $validator->getMessageBag(),
+        ], 400));
+    }
+}

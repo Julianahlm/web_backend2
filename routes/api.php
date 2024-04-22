b80dd03a855461d0b69b62885001da26f3fad1a4
Julianahlm <165674015+Julianahlm@users.noreply.github.com>
2024-04-22 14:34:35 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//   return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
 // });
-route::group(['prefix'=>'v1'],function(){
-    // api/v1/users
-    route::post('/users',[App\Http\Controller::class,'register']);
-    route::post('/users/login',[App\Http\Controllers\userController::class,'login']);
+Route::group(['prefix'=>'v1'],function(){
+    Route::post('/users',[App\Http\Controllers\UserController::class,'register']);
+    Route::post('/users/login',[App\Http\Controllers\UserController::class,'login']);
+
+    Route::middleware(App\Http\Middleware\ApiAuthMiddleware::class)->group(function (){
+        Route::get('/users/profile',[App\Http\Controllers\UserController::class,'get']);
+        Route::patch('/users/profile',[App\Http\Controllers\UserController::class,'update']);
+        Route::delete('/users/logout',[App\Http\Controllers\UserController::class,'logout']);
+    });
+
+    Route::post('/contacts', [App\Http\Controllers\ContactsController::class, 'create']);
+    Route::patch('/contacts/{id}', [App\Http\Controllers\ContactsController::class, 'update']);
+    Route::delete('/contacts/{id}', [App\Http\Controllers\ContactsController::class, 'delete']);
+    Route::get('/contacts/search', [App\Http\Controllers\ContactsController::class, 'search']);
 });
